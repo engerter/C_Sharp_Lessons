6abf14863778264585547af41691ea1d468c681c
engerter <alekseykrugloff@gmail.com>
2023-04-21 14:31:13 +0300

    Solution of the second task

---
+++ task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.WriteLine("Введите три числа:");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+int number_2 = Convert.ToInt32(Console.ReadLine());
+int number_3 = Convert.ToInt32(Console.ReadLine());
+int max_number = number_1;
+if (number_2 > max_number)
+{
+    max_number = number_2;
+}
+if(number_3 > max_number)
+{
+    max_number = number_3;
+}
+Console.WriteLine("Наибольшее число: " + max_number);
