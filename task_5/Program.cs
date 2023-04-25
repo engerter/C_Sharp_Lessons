2e7a7bafe2a06b8e2d8288dab49f04df0d8e500b
engerter <alekseykrugloff@gmail.com>
2023-04-25 14:01:54 +0300

    Solution of the fifth task

---
+++ task_5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number < 1000)
+{
+    int second_num = (number % 100 - number % 10) / 10;
+    Console.WriteLine("Вторая цифра числа "+number+" это "+second_num);
+}
+else
+    System.Console.WriteLine("Это число не трехзначное");
