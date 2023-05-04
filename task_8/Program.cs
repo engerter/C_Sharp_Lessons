14c60c86ccb3fcfd6dbabb50a152f854ee01c796
engerter <alekseykrugloff@gmail.com>
2023-05-04 14:34:45 +0300

    Solution of the eighth task

---
+++ task_8/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int num_len = number.Length;
+
+if (num_len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine(number+" (ДА) - является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine(number+" (НЕТ) - не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine(number+ " не является пятизначным");
+}
