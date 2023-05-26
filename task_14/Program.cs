dace83e9cb33cd49fb29a9e6661615b3fead4183
engerter <alekseykrugloff@gmail.com>
2023-05-26 13:12:42 +0300

    Solution of the fourteenth task

---
+++ task_14/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.Clear();
+Random rnd = new Random();
+int[] numbers = new int[10];
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = rnd.Next(100, 1000);
+}
+
+Console.Write("Массив случайных трехзначных чисел: ");
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
+Console.WriteLine();
+
+int count = CountEvenNumbers(numbers);
+Console.WriteLine($"Количество четных чисел в массиве: {count}");
+
+int CountEvenNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
