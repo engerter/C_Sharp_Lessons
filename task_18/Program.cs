98514101e9f91de0405934e9eb4059f9e5606d69
engerter <alekseykrugloff@gmail.com>
2023-05-31 13:18:24 +0300

    Solution of the eighteenth task

---
+++ task_18/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Clear();
+
+Console.Write("Введите значение k1: ");
+double k1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение b1: ");
+double b1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение k2: ");
+double k2 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение b2: ");
+double b2 = double.Parse(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения двух прямых: ({x}, {y})");
