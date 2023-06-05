cd766d63cb7376f55eb9b16bf2531a434eba44ea
engerter <alekseykrugloff@gmail.com>
2023-06-05 12:31:16 +0300

    Solution of the nineteenth task

---
+++ task_19/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами. m = 3, n = 4.
+
+Console.Clear();
+
+int m = 3;
+int n = 4;
+
+double[,] arr = new double[m, n];
+Random rand = new Random();
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        arr[i, j] = Math.Round(rand.NextDouble() * 20 - 10, 1);
+    }
+}
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
