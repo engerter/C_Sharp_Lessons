9724d8c63ea2c054148f77739057e443da33d761
engerter <alekseykrugloff@gmail.com>
2023-06-08 14:21:01 +0300

    Solution of the twenty seventh task

---
+++ task_27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+
+Console.WriteLine("Введите значение M:");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение N:");
+int N = int.Parse(Console.ReadLine());
+
+int sum = Sum(M, N);
+
+Console.WriteLine("Сумма натуральных чисел от {0} до {1} равна {2}", M, N, sum);
+
+int Sum(int M, int N)
+{
+    if (M == N)
+    {
+        return M;
+    }
+    else
+    {
+        return Sum(M, N - 1) + N;
+    }
+}
